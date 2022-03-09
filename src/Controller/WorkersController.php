1a3eb450ee3158fc82240ff8982b09d5ec2d1e0a
quatrecouleurs <joel.obrecht@gmail.com>
2022-03-09 15:51:53 +0000

    Proof of concept controller/twig

--- src/Controller/WorkersController.php
+++ src/Controller/WorkersController.php
@@ -13,8 +13,24 @@ class WorkersController extends AbstractController
      */
     public function index(): Response
     {
+
+        //~ URL appelée nous retournant des données au format JSON
+        $data_url = 'http://api.randomuser.me/?results=12';
+
+        //~ On appelle l'URL et on stocke le contenu retourné dans une variable
+        $data_contenu = file_get_contents($data_url);
+
+        //~ Les données étant au format JSON, on les décode pour les stocker sous la forme d'un tableau associatif
+        $data_array = json_decode($data_contenu, true);
+
+        //~ On pointe directement sur les données du/des utilisateur(s) retourné(s)
+        $utilisateurs = $data_array['results'];
+
+        //dd($utilisateurs);
+
         return $this->render('workers/index.html.twig', [
             'controller_name' => 'WorkersController',
+            "workers" => $utilisateurs,
         ]);
     }
 }
